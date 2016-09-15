e74b3237187f8cc2ae625ab05471aaa9605d8da9
colin millar <colinpmillar@gmail.com>
2016-09-15 10:31:19 +0200

    added package help file

---
+++ R/icesVocab-package.R
@@ -0,0 +1,29 @@
+#' @docType package
+#'
+#' @name icesVocab-package
+#'
+#' @aliases icesVocab
+#'
+#' @title ICES Vocabularies Database Web Services
+#'
+#' @description
+#' R interface to access the RECO POX web services of the ICES (International
+#' Council for the Exploration of the Sea) Vocabularies
+#' database <http://vocab.ices.dk/services/POX.aspx>.
+#'
+#' @details
+#' \emph{Get dataset:}
+#' \tabular{ll}{
+#'   \code{\link{getCodeListType}} \tab retrieves all the code types\cr
+#'   \code{\link{getCodeList}} \tab retrieves all the codes for the given code type\cr
+#'   \code{\link{getCodeDetail}} \tab retrieves all the details for the given code\cr
+#' }
+#'
+#' @author Colin Millar.
+#'
+#' @references
+#' ICES Vocabularies RECO POX web services:
+#' \url{http://vocab.ices.dk/services/POX.aspx}.
+#'
+
+NA
